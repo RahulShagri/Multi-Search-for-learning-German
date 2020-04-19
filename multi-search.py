e0f6625e1b56cb17c9ddfa00a5e1ecc758f1984c
RahulShagri <62894667+RahulShagri@users.noreply.github.com>
2020-04-19 12:49:39 +0530

    Update multi-search.py

--- multi-search.py
+++ multi-search.py
@@ -1,4 +1,5 @@
 import pyautogui
+import pyperclip
 
 def locate_wait(img):
     loc = None
@@ -14,25 +15,39 @@ while True:
         break
 
     user_word = user_word.lower()
+    pyperclip.copy(user_word)
     pyautogui.moveTo(pyautogui.center(locate_wait("chrome.png")), duration = 0.2)
     pyautogui.click()
 
     pyautogui.hotkey('ctrl', 't')
-    pyautogui.typewrite("https://www.google.de/search?q=" + user_word + "&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjs8Mf514_oAhXCWisKHU-FDAQQ_AUoA3oECAsQBQ&biw=1366&bih=657", interval = 0.005)
+    pyautogui.typewrite("https://www.dict.cc/?s=", interval = 0.005)
+    pyautogui.hotkey('ctrl', 'v')
     pyautogui.press('enter')
 
     pyautogui.hotkey('ctrl', 't')
-    pyautogui.typewrite("https://en.wiktionary.org/w/index.php?title="+ user_word +"&rdfrom", interval = 0.005)
+    pyautogui.typewrite("https://www.google.de/search?q=", interval = 0.005)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.typewrite("&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjs8Mf514_oAhXCWisKHU-FDAQQ_AUoA3oECAsQBQ&biw=1366&bih=657", interval = 0.005)
     pyautogui.press('enter')
 
     pyautogui.hotkey('ctrl', 't')
-    pyautogui.typewrite("https://de.thefreedictionary.com/" + user_word, interval = 0.005)
+    pyautogui.typewrite("https://en.wiktionary.org/w/index.php?title=", interval = 0.005)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.typewrite("&rdfrom", interval = 0.005)
     pyautogui.press('enter')
 
     pyautogui.hotkey('ctrl', 't')
-    pyautogui.typewrite("https://www.linguee.com/english-german/search?source=auto&query=" + user_word, interval = 0.005)
+    pyautogui.typewrite("https://de.thefreedictionary.com/", interval = 0.005)
+    pyautogui.hotkey('ctrl', 'v')
     pyautogui.press('enter')
 
     pyautogui.hotkey('ctrl', 't')
-    pyautogui.typewrite("https://de.forvo.com/word/"+ user_word +"/#de", interval = 0.005)
+    pyautogui.typewrite("https://www.linguee.com/english-german/search?source=auto&query=", interval = 0.005)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.press('enter')
+
+    pyautogui.hotkey('ctrl', 't')
+    pyautogui.typewrite("https://de.forvo.com/word/", interval = 0.005)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.typewrite("/#de", interval = 0.005)
     pyautogui.press('enter')
